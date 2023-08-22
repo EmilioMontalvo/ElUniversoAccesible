0e991a05f3c2be17be03e0d037f2f87ecb3af0f7
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2023-08-21 23:18:28 -0500

    fin header

--- ElUniverso/js/script.js
+++ ElUniverso/js/script.js
@@ -6,12 +6,36 @@ const setTheme = (theme) => {
     localStorage.setItem('theme', theme);
 }
 
-slider.addEventListener('click', ()  => {
+slider.addEventListener('click', () => {
+    toggleTheme();
+});
+
+slider.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Evitar que el formulario se envíe por defecto
+    }
+});
+
+slider.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        toggleTheme();
+    }
+});
+
+function toggleTheme() {
     let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
     setTheme(switchToTheme);
-});
+}
 
 setTheme(localStorage.getItem('theme') || preferedColorScheme);
 
 
+const themeToggle = document.querySelector('.theme-toggle');
+
+themeToggle.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        themeToggle.checked = !themeToggle.checked; // Cambiar el estado del switch
+        event.preventDefault(); // Prevenir el comportamiento por defecto
+    }
+});
 
