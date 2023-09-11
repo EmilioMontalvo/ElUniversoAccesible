84e43d3e31fd4351e6ef6a6b71d6b477e06dbc45
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2023-09-11 13:33:21 -0500

    tamaño de letra arreglado

--- ElUniverso/js/aumentartexto.js
+++ ElUniverso/js/aumentartexto.js
@@ -5,11 +5,11 @@ const contenidoTexto = document.querySelector('.contenidoTexto');
 botonAumentar.addEventListener('click', () => {
     const computedStyle = getComputedStyle(document.documentElement);
     const textSize = parseFloat(computedStyle.getPropertyValue('--text-size'));
-    document.documentElement.style.setProperty('--text-size', (textSize + 0.2) + 'rem');
+    document.documentElement.style.setProperty('--text-size', (textSize + 0.2));
 });
 
 botonDisminuir.addEventListener('click', () => {
     const computedStyle = getComputedStyle(document.documentElement);
     const textSize = parseFloat(computedStyle.getPropertyValue('--text-size'));
-    document.documentElement.style.setProperty('--text-size', (textSize - 0.2) + 'rem');
+    document.documentElement.style.setProperty('--text-size', (textSize - 0.2));
 });
