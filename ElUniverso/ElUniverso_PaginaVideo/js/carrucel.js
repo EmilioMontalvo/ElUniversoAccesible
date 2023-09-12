16680ee9d657bc2068c698debc78d78c506500d7
David León <94190370+Dasesleo@users.noreply.github.com>
2023-09-12 00:06:43 -0500

    Pagina con video

---
+++ ElUniverso/ElUniverso_PaginaVideo/js/carrucel.js
@@ -0,0 +1,13 @@
+const botonIniciarSesion = document.getElementById("botonIniciarSesion");
+
+botonIniciarSesion.addEventListener("click", function() {
+  // Redirige a la página deseada
+  window.location.href = "../ElUniverso_Registro/login.html"; // Cambia "otra_pagina.html" por la URL de la página a la que deseas redirigir
+});
+
+const botonRegistrarse = document.getElementById("botonRegistrarse");
+
+botonRegistrarse.addEventListener("click", function() {
+  // Redirige a la página deseada
+  window.location.href = "../ElUniverso_Registro/registro.html"; // Cambia "otra_pagina.html" por la URL de la página a la que deseas redirigir
+});
